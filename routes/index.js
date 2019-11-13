940c7343e545a61b5361e93cddef23ee8c42a4b2
doriorio <itsdori@gmail.com>
2019-11-13 15:22:55 -0800

    bootstrapping

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req,res){
     res.render('index',
-    { title: 'Express To-Do' });
+    { title: 'Dev Skills' });
 });
 
 module.exports = router;
